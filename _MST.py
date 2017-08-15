fcfdaee3450c8c68aac49d6799faa05e5dbe91d6
Ryan Cybulski <ryan@Ryans-MacBook-Pro-5.local>
2017-08-15 15:47:22 -0400

    Added external VT and MST class files

---
+++ _MST.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import seaborn as sns
+
+import pylab as pl
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import imageio
+import warnings; warnings.filterwarnings('ignore', message='elementwise')
+import mpl_toolkits.axisartist.angle_helper as angle_helper
+from matplotlib.projections import PolarAxes
+from mpl_toolkits.axisartist.grid_finder import (FixedLocator, MaxNLocator,
+                                                 DictFormatter)
+from matplotlib.transforms import Affine2D
+import mpl_toolkits.axisartist.floating_axes as floating_axes
+
+#### MST clustering library and astropy for coordinate transforms
+from mst_clustering import MSTClustering
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+sns.set_style("whitegrid")
+
+class MST:
+    """ Minimum Spanning Tree Class
+    
+    Compute MST for a set of input points using the MSTClustering 
+    code from jakevdp, calculate branch lengths from the MST and 
+    generate plots of the MST and cumulative distribution of branch 
+    lengths.
+    
+    ---- Inputs ----
+    data frame "df", which has two columns present:
+     - ra: right ascension (deg)
+     - dec: declination (deg)
+    
+    cutoff_scale (float): minimum size of edges. All edges larger 
+                          than cutoff_scale will be removed.
+    
+    min_cluster_size (int): min number of galaxies in a cluster.
+    
+    n_neighbors (int): maximum number of neighbors of each point 
+    used for approximate Euclidean MST algorithm.
+    
+    
+    ---- Attributes ----
+    labels: integer specifying the structure to which a given galaxy 
+            has been assigned. It will have a -1 if no membership was 
+            assigned.
+            
+    segments: sets of ra, dec coordinates for the MST branch segments
+    seps: base-10 log of branch lengths (in degrees)
+    
+    """
+    def __init__(self, df, cutoff_scale=None, min_cluster_size=None, 
+                 n_neighbors=None, set_mst=None, labels=None, 
+                 segments=None, seps=None):
+        self.df = df
+        self.cutoff_scale = cutoff_scale
+        self.min_cluster_size = min_cluster_size
+        self.n_neighbors = n_neighbors
+        self.set_mst = MSTClustering(cutoff_scale=cutoff_scale, 
+                             min_cluster_size=min_cluster_size, 
+                             n_neighbors=n_neighbors)
+        pos = np.array([list(i) for i in zip(df.ra, df.dec)])
+        self.labels = self.set_mst.fit_predict(pos)
+        self.segments = self.set_mst.get_graph_segments(full_graph=True)
+        self.seps = self.get_sep_mst()
+        
+    """ Calculate branch lengths (in base-10 log(degrees)) 
+        from the MST segments """    
+    def get_sep_mst(self):
+        mst_coord0_ra = np.asarray(self.segments[0][0])
+        mst_coord1_ra = np.asarray(self.segments[0][1])
+        mst_coord0_dec = np.asarray(self.segments[1][0])
+        mst_coord1_dec = np.asarray(self.segments[1][1])
+        c0 = SkyCoord(mst_coord0_ra, mst_coord0_dec, unit=u.deg)
+        c1 = SkyCoord(mst_coord1_ra, mst_coord1_dec, unit=u.deg)
+        return np.log10(c0.separation(c1).degree*3600.0)
+        
+    """ Plot the MST diagram (left) and the labeled structures 
+        identified from the MST (right) """
+    def plot_mst(self, model, cmap='rainbow', *args, **kwargs):
+        """Utility code to visualize a minimum spanning tree"""
+        xlim = kwargs.get('xlim', None)
+        ylim = kwargs.get('ylim', None)
+        ssize = kwargs.get('s', 8)
+        X = model.X_fit_
+        fig, ax = plt.subplots(1, 2, figsize=(20, 7), sharex=True, sharey=True)
+        for axi, full_graph, colors in zip(ax, [True, False], ['lightblue', model.labels_]):
+            segments = model.get_graph_segments(full_graph=full_graph)
+            axi.plot(segments[0], segments[1], '-k', zorder=1, lw=1)
+            plt.xlabel('Right Ascension (deg)', size=14)
+            plt.ylabel('Declination (deg)', size=14)
+            axi.scatter(X[:, 0], X[:, 1], c=colors, cmap=cmap, zorder=2, s=ssize)
+            axi.axis('tight')
+            if xlim != None:
+                plt.xlim(xlim)
+            if ylim != None:
+                plt.ylim(ylim)
+    
+        ax[0].set_title('Full Minimum Spanning Tree', size=16)
+        ax[1].set_title('Trimmed Minimum Spanning Tree', size=16);
+        
+    """ Plot the cumulative distribution of MST branch lengths """
+    def plot_mst_cumul(self):
+        sns.distplot(self.seps, hist_kws=dict(cumulative=False), 
+                     kde_kws=dict(cumulative=True))
+        plt.xlabel('log$_{10}$ (MST branch length)', fontsize=15)
+        plt.ylabel('Norm. Counts/Cumul. Dist.', fontsize=15)
---
+++ _VT.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import scipy
+import seaborn as sns
+
+import pylab as pl
+from scipy.spatial import Voronoi
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import imageio
+import warnings; warnings.filterwarnings('ignore', message='elementwise')
+import mpl_toolkits.axisartist.angle_helper as angle_helper
+from matplotlib.projections import PolarAxes
+from mpl_toolkits.axisartist.grid_finder import (FixedLocator, MaxNLocator,
+                                                 DictFormatter)
+from matplotlib.transforms import Affine2D
+import mpl_toolkits.axisartist.floating_axes as floating_axes
+
+#### MST clustering library and astropy for coordinate transforms
+sns.set_style("whitegrid")
+
+""" Compute area of a polynomial given input vertices """
+def get_polyarea(x, y):
+    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+
+class VT:
+    """ Voronoi Tessellation Class 
+    
+    Compute Voronoi tessellation on a set of input points using 
+    scipy's Voronoi function.
+    
+    ---- Input ----
+    data frame "df", which has two columns present:
+     - ra: right ascension (deg)
+     - dec: declination (deg)
+     
+    ---- Attributes ----
+    celldensity: base-10 log of surface density for each input 
+    galaxy (in units of 1/[sq. deg]), calculated from the inverse 
+    of the cell area
+    
+    """
+    def __init__(self, df, celldensity=None):
+        self.df = df
+        self.celldensity = celldensity
+        self.get_vt(df)
+
+    """ Run scipy Voronoi function """
+    def get_vt(self, df):
+        pos = np.array([list(i) for i in zip(df.ra, df.dec)])
+        return Voronoi(pos)
+        
+    """ Calculate cell densities """
+    def get_celldens(self, V):
+        cellx = []
+        celly = []
+        holdx = []
+        holdy = []
+        celldens = []
+        for reg in V.regions:
+            if -1 in reg:
+                cellx.append([-1])
+                celly.append([-1])
+                celldens.append(-99)
+            else:
+                for num in reg:
+                    holdx.append(V.vertices[num][0])
+                    holdy.append(V.vertices[num][1])
+                cellx.append(holdx)
+                celly.append(holdy)
+                cellarea = get_polyarea(holdx, holdy)
+                if cellarea == 0:
+                    # Some rare bad cells with area=0
+                    # Set those to null density
+                    celldens.append(-99)
+                else:
+                    celldens.append(np.log10(1.0/float(cellarea)))
+                holdx = []
+                holdy = []
+        return celldens
+    
+    """ Visualize the Voronoi cells """
+    def plot_voronoi(self, V, *args, **kwargs):
+        xlim = kwargs.get('xlim', None)
+        ylim = kwargs.get('ylim', None)
+        plt.figure(figsize=(14,8))
+        for vind in V.ridge_vertices:
+            (i1, i2) = sorted(vind)
+            if (i1 != -1) & (i2 != -1):
+                vor1 = V.vertices[i1]
+                vor2 = V.vertices[i2]
+                p, = plt.plot([vor1[0], vor2[0]], [vor1[1], vor2[1]], 'k-')
+        plt.xlabel('Right Ascension (deg)', fontsize=18)
+        plt.ylabel('Declination (deg)', fontsize=18)
+        if xlim != None:
+            plt.xlim(xlim)
+        if ylim != None:
+            plt.ylim(ylim)
+            
+    """ Plot the cumulative distribution of VT cell densities """
+    def plot_vt_cumul(self, V):
+        vcalc = np.asarray(V.celldensity)
+        celldens_calc = vcalc[vcalc > -99]
+        sns.distplot(celldens_calc, hist_kws=dict(cumulative=False), 
+                     kde_kws=dict(cumulative=True))
+        plt.xlabel('log$_{10}$ (VT Cell Densities)', fontsize=15)
+        plt.ylabel('Norm. Counts/Cumul. Dist.', fontsize=15)
